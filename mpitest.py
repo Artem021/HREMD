c4103c19fedc16579a39dcf9262cf968a1adea0a
Artem021 <bananafriend21@gmail.com>
2024-12-11 19:25:41 +0300

    remove files with developing features

--- mpitest.py
+++
@@ -1,6 +0,0 @@
-from engines import runMD
-import re,os,shutil
-
-arg = ('/home/artem/LAMMPS_TEST/macro/10.07/0.000e+00', 'lammps.inp', '/home/artem/LAMMPS_TEST/macro.data', 'lammps.error', 'traj.xyz', 10, re.compile('Time\\s+Temp\\s+PotEng\\s+KinEng\\s+Press\\s+Volume\\s+Density'), 2)
-
-res = runMD(arg)
